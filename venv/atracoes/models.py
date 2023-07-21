a7d6d34cc2d7305572ed239a10a0f2dac6443c93
Carlos Roberto <carlosrjhoe@gmail.com>
2023-07-21 05:02:37 -0300

    Django Rest: Adicionando a propriedade blanck, no models de atrações

--- venv/atracoes/models.py
+++ venv/atracoes/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Atracao(models.Model):
-    nome = models.CharField(max_length=150)
-    descricao = models.TextField()
-    horario_func = models.TextField()
-    idade_minima = models.IntegerField()
+    nome = models.CharField(max_length=150, blank=True)
+    descricao = models.TextField(blank=True)
+    horario_func = models.TextField(blank=True)
+    idade_minima = models.IntegerField(blank=True)
 
     def __str__(self):
         return self.nome
--- venv/ponto_turistico/models.py
+++ venv/ponto_turistico/models.py
@@ -5,13 +5,13 @@ from avaliacoes.models import Avaliacao
 from endereco.models import Endereco
 # Create your models here.
 class PontoTuristico(models.Model):
+    endereco = models.ForeignKey(Endereco, on_delete=models.CASCADE)
     nome = models.CharField(max_length=150)
     descricao = models.TextField()
     aprovado = models.BooleanField(default=False)
     atracoes = models.ManyToManyField(Atracao, blank=True)
     comentarios = models.ManyToManyField(Comentario, blank=True)
     avaliacoes = models.ManyToManyField(Avaliacao, blank=True)
-    endereco = models.ForeignKey(Endereco, on_delete=models.CASCADE)
 
     def __str__(self) -> str:
         return self.nome
