d6d88c8c0d05cd3f697e50252cbcb43ed2df2df2
Carlos Roberto <carlosrjhoe@gmail.com>
2023-07-28 04:50:43 -0300

    Django-Rest: configurando app comentarios

--- django/comentarios/models.py
+++ django/comentarios/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Comentario(models.Model):
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    comentario = models.TextField()
+    data = models.DateTimeField(auto_now_add=True)
+    aprovado = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return self.usuario.username
