83bf8836820a4c9df9d1aae71195d6245e08518d
Carlos Roberto <carlosrjhoe@gmail.com>
2023-08-08 20:26:17 -0300

    Django-Rest: Sobrescrevendo metodo GET

--- django/ponto_turistico/api/viewsets.py
+++ django/ponto_turistico/api/viewsets.py
@@ -3,7 +3,6 @@ from ponto_turistico.models import PontoTuristico
 from .serializers import PontoTuristicoSerializer
 
 class PontoTuristicoViewSet(ModelViewSet):
-    # queryset = PontoTuristico.objects.all()
     serializer_class = PontoTuristicoSerializer
 
     def get_queryset(self):
