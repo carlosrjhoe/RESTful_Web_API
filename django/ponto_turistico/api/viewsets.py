b20fccb5f4c33fae2dc7fa5b59bc5b28dcddbfd1
Carlos Roberto <carlosrjhoe@gmail.com>
2023-08-27 21:26:30 -0300

    Django-Rest: Aplicando mecanismo de autorização

--- django/ponto_turistico/api/viewsets.py
+++ django/ponto_turistico/api/viewsets.py
@@ -1,8 +1,10 @@
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 from ponto_turistico.models import PontoTuristico
 from .serializers import PontoTuristicoSerializer
 
 class PontoTuristicoViewSet(ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = PontoTuristico.objects.all()
     serializer_class = PontoTuristicoSerializer
 
