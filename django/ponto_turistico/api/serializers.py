5adaf4bed8084d951886b9e51fa0c1dd34623295
Carlos Roberto <carlosrjhoe@gmail.com>
2023-08-29 19:42:51 -0300

    Django-Rest: Incrementando um objeto com NestedRelationships

--- django/ponto_turistico/api/serializers.py
+++ django/ponto_turistico/api/serializers.py
@@ -1,8 +1,11 @@
 from rest_framework.serializers import ModelSerializer
 from ponto_turistico.models import PontoTuristico
+from atracoes.api.serializers import AtracaoSerializer 
 
 class PontoTuristicoSerializer(ModelSerializer):
+
+    atracoes = AtracaoSerializer(many=True, read_only=True)
     class Meta:
         model = PontoTuristico
-        fields = ['id', 'nome', 'descricao']
+        fields = ['id', 'nome', 'descricao', 'atracoes']
         
